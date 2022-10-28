96e0408c16bd2532695e2ecd04b37e5e8b2ba2f1
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2022-10-28 19:01:31 +0200

    Projet Conway
    Jeu de la vie (Conway) Matlab

---
+++ jdlv_conway.m
@@ -0,0 +1,33 @@
+N=100;
+N = N + 1;
+M = randi([0,1],N);
+M(:,1)=0;
+M(:,N)=0;
+M(N,:)=0;
+M(1,:)=0;
+G = M;
+
+%M = rand(N,N)>0.8 "dillution de proba"
+for o = 1:1000000
+    spy(M)
+    for i = 1:N-2
+        for j = 1:N-2
+            k = sum(sum(M(i:2+i,j:2+j)));
+            if M(i+1,j+1) == 1
+                k = k-1;
+            end
+            if k == 3
+                G(i+1,j+1) = 1;
+            elseif k > 3 
+                G(i+1,j+1) = 0;
+            elseif k == 2
+                G(i+1,j+1) = M(i+1,j+1);
+            else
+                G(i+1,j+1) = 0;
+            end 
+        end
+    end
+    spy(M); drawnow
+    M = G;
+end
+
