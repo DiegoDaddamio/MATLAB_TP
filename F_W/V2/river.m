f4d95db048e552247386c5db534e42a52e09f175
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2023-09-03 06:01:56 +0200

    Update river.m

--- F_W/V2/river.m
+++ F_W/V2/river.m
@@ -1,12 +1,12 @@
 function [matrice2] = river(SizeWorld,matrice)
 World_river = zeros(SizeWorld*3);
-for i = 1:randi(5)
+for i = 1:randi(floor(SizeWorld/23))
     River_First_x = randi([SizeWorld,SizeWorld*2],1,1);
     River_First_y = randi([SizeWorld,SizeWorld*2],1,1);
     World_river(River_First_x-1:River_First_x+1,River_First_y-1:River_First_y+1) = 10;
     World_river(River_First_x-2:River_First_x+2,River_First_y) = 10;
     World_river(River_First_x,River_First_y-2:River_First_y+2) = 10;
-    for k=1:1000
+    for k=1:SizeWorld*15
         switch(randi(4))
             case 1
                 River_First_x = River_First_x + randi(2);
